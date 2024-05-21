c0412ed813fdc462f160e62c71d69de5ff368645
Gabriel Vieira Teodoro <teodorogabriel367@gmail.com>
2024-05-20 22:21:24 -0300

    Feat(BD): sequelize config file

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,1 +1,18 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize } from 'sequelize-typescript';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//Make sure to create a .env file in the root folder
+
+const sequelize = new Sequelize({
+  database: process.env.POSTGRESS_DATABASE,
+  dialect: 'postgres',
+  username: process.env.POSTGRESS_USER,
+  password: process.env.POSTGRESS_PASSWORD,
+  host: process.env.POSTGRESS_HOST,
+  port: Number(process.env.POSTGRESS_PORT), 
+  models: [__dirname + '/models'],
+});
+
+export default sequelize;
