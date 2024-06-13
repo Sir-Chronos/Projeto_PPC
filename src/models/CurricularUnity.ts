11e8c3fb9c21c464c36ee1c372247c6ec3fe7aa1
Gabriel Vieira Teodoro <teodorogabriel367@gmail.com>
2024-06-13 09:00:21 -0300

    feat: models re completed

--- src/models/CurricularUnity.ts
+++ src/models/CurricularUnity.ts
@@ -1,10 +1,16 @@
-import { Model, Table, Column, DataType, BelongsToMany } from 'sequelize-typescript';
+import { Model, Table, Column, DataType, BelongsTo, BelongsToMany, ForeignKey } from 'sequelize-typescript';
+import PPC from './PPC';
 import Bibliograph from './Bibliograph';
+import Knowledge from './Knowledge';
+import Skill from './Skill';
 import CurricularUnityBibliograph from './CurricularUnityBibliograph';
+import CurricularUnityKnowledge from './CurricularUnityKnowledge';
+import CurricularUnitySkill from './CurricularUnitySkill';
 
 interface CurricularUnityAttributes {
   objective: string;
   name: string;
+  ppcId: number;  // Foreign key to PPC
 }
 
 @Table({ tableName: 'curricularUnity' })
@@ -21,7 +27,25 @@ export default class CurricularUnity extends Model<CurricularUnityAttributes> im
   })
   name!: string;
 
+  @ForeignKey(() => PPC)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  ppcId!: number;
+
+  @BelongsTo(() => PPC)
+  ppc!: PPC;
+
   // Define the many-to-many relationship with Bibliograph
   @BelongsToMany(() => Bibliograph, () => CurricularUnityBibliograph)
   bibliographs!: Bibliograph[];
+
+  // Define the many-to-many relationship with Knowledge
+  @BelongsToMany(() => Knowledge, () => CurricularUnityKnowledge)
+  knowledges!: Knowledge[];
+
+  // Define the many-to-many relationship with Skill
+  @BelongsToMany(() => Skill, () => CurricularUnitySkill)
+  skills!: Skill[];
 }
